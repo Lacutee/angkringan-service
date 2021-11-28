6c390eb34098f1f6bf542ea861eb93f5b392a426
Ridho Muhammad <5mblackburn7@gmail.com>
2021-11-28 19:03:39 +0700

    Update index.js

--- controllers/hist/index.js
+++ controllers/hist/index.js
@@ -13,6 +13,7 @@ module.exports = {
         const nama = req.body.nama;
         const suhu = req.body.suhu;
         const jenis = req.body.jenis;
+        const rasa = req.body.rasa;
         const ukuran = req.body.ukuran;
         const Tambahan = req.body.Tambahan;
         const jumlah = req.body.jumlah;
